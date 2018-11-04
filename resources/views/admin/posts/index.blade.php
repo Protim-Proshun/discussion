816b39af03fe0723b85ae90d470af72f7af67993
Protim <36602930+Protim-Proshun@users.noreply.github.com>
2018-11-04 23:04:55 +0600

    first

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+    <h1>Posts</h1>
+
+    <table class="table">
+       <thead>
+         <tr>
+             <th>Id</th>
+             <th>Photo</th>
+             <th>User</th>
+             <th>Category</th>
+             <th>Title</th>
+             <th>Body</th>
+             <th>Post link</th>
+             <th>Comments</th>
+             <th>Created</th>
+             <th>Updated</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        @if($posts)
+
+            @foreach($posts as $post)
+
+            <tr>
+                <td>{{$post->id}}</td>
+                <td>{{$post->photo ? $post->photo->file : 'https://www.w3schools.com/images/w3schools_green.jpg'}}</td>
+                <td><a href="{{route('admin.posts.edit', $post->id)}}">{{$post->user->name}}</a></td>
+                <td>{{$post->category ? $post->category->name : 'Uncategorized'}}</td>
+                <td>{{$post->title}}</td>
+                <td>{{$post->body}}</td>
+                <td><a href="{{route('home.post', $post->id)}}">View Post</a></td>
+                <td><a href="{{route('admin.comments.show', $post->id)}}">View Comments</a></td>
+                <td>{{$post->created_at->diffForHumans()}}</td>
+                <td>{{$post->updated_at->diffForHumans()}}</td>
+            </tr>
+
+            @endforeach
+
+        @endif
+
+       </tbody>
+     </table>
+
+@stop
